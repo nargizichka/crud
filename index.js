94ba0ae6cc760bbaf97c1842be8ba97043c34059
nargizichka <rustamxonovanargiza2005@gmail.com>
2025-01-07 18:41:51 +0500

    Initial Commit

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config(); // dotenv paketini chaqirish
+
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require("cors"); 
+const cors = require("cors");
 const ProductRoutes = require("./routes/products");
 const CategoryRoutes = require("./routes/category");
 const UserRouter = require("./routes/users");
@@ -13,12 +15,12 @@ app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.use(cors({
-    origin: "http://localhost:5173", 
-    methods: ["GET", "POST", "PUT", "DELETE"], 
-    credentials: true, 
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
 }));
 
-const url = "mongodb+srv://rustamxonovanargiza:nargizaxon@cluster0.hcpu1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+const url = process.env.MONGO_URI;
 
 mongoose.connect(url)
   .then(() => console.log("Connected to DB"))
@@ -27,4 +29,6 @@ mongoose.connect(url)
 app.use("/products", ProductRoutes);
 app.use("/category", CategoryRoutes);
 app.use("/auth", UserRouter);
-app.listen(3000, () => console.log('Server started on port 3000'));
+
+const PORT = 3000;
+app.listen(PORT, () => console.log('Server started on port 3000'));
